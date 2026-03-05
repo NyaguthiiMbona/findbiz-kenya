a831e733a0b67b6d9f0ff80b2b6d89bcb345aa17
Ruth Njaramba <rnjaramba@gmail.com>
2026-03-05 18:51:48 +0300

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -66,13 +66,13 @@ export const generateSitemap = functions.pubsub
       .get();
     
     let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+`<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
     
     businesses.forEach(doc => {
       const data = doc.data();
       sitemap += `
   <url>
-    <loc>https://findbiz.co.ke/business/${doc.id}</loc>
+    `<loc>https://findbiz.co.ke/business/${doc.id}</loc>`;
     <lastmod>${data.updatedAt.toDate().toISOString()}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>${data.isFeatured ? '0.8' : '0.6'}</priority>
